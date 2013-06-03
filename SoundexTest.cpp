b05ff37326121f205a3053850c51f4581002f60e
Mark Sanders <mark.a.sanders@capgemini.com>
2013-06-03 14:30:46 +0100

    Adding code to encode consonants to digits

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <unordered_map>
+
 #include "gmock/gmock.h"    
 
 using ::testing::Eq;
@@ -13,22 +16,33 @@ public:
     }
 
 private:
-
+    static const unsigned int MaxCodeLength { 4 };
+    
+    const std::unordered_map<char, std::string> encodings {
+        { 'b', "1" },
+        { 'c', "2" },
+        { 'd', "3" },
+    };
+    
     std::string head(const std::string& word) const {
         return word.substr(0, 1);
     };
 
+    std::string encodedDigit(char letter) const {
+    	return encodings.find(letter)->second;
+    };
+
     std::string encodedDigits(const std::string& word) const {
         if (word.length() > 1) {
-            return "1";
+            return encodedDigit(word[1]);
         }
         else {
-            return "0";
+            return "";
         }
     };
 
     std::string zeroPad(const std::string& code) const {
-        auto zerosNeeded = 4 - code.length();
+        auto zerosNeeded = MaxCodeLength - code.length();
         return code + std::string(zerosNeeded, '0');
     }
 };
@@ -49,5 +63,7 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
 
 TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) { 
     ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+    ASSERT_THAT(soundex.encode("Ac"), Eq("A200"));
+    ASSERT_THAT(soundex.encode("Ad"), Eq("A300"));
 }
 
