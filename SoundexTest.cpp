8e7b754aa7f06bad22a04e0ddb2843b59324b9b6
Mark Sanders <mark.a.sanders@capgemini.com>
2013-06-03 13:30:47 +0100

    Adding zero padding

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -5,7 +5,12 @@ using ::testing::Eq;
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return "A";
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string& word) const {
+        return word + "000";
     }
 };
 
@@ -14,8 +19,13 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     
     auto encoded = soundex.encode("A");
     
-    ASSERT_THAT(encoded, Eq("A"));
-
-
+    ASSERT_THAT(encoded, Eq("A000"));
 }
 
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    Soundex soundex;
+    
+    auto encoded = soundex.encode("I");
+    
+    ASSERT_THAT(encoded, Eq("I000"));
+}
