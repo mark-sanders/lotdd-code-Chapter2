7378bf557379bc81206360480ad4c54f770ca401
Mark Sanders <mark.a.sanders@capgemini.com>
2013-06-03 13:38:38 +0100

    Simplifying

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -25,13 +25,9 @@ public:
 };
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { 
-    auto encoded = soundex.encode("A");
-    
-    ASSERT_THAT(encoded, Eq("A000"));
+    ASSERT_THAT(soundex.encode("A"), Eq("A000"));
 }
 
 TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-    auto encoded = soundex.encode("I");
-    
-    ASSERT_THAT(encoded, Eq("I000"));
+    ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
