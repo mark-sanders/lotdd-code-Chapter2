3c15f5b32c7774bad1c96af6bfb8145d328a1928
Mark Sanders <mark.a.sanders@capgemini.com>
2013-06-04 16:10:58 +0100

    Run tests from book, fixed issue with Jbob - DoesNotCombineDuplicateEncodingsSeparatedByVowels

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -13,10 +13,10 @@ private:
 public:
     static std::string encode(const std::string& word) {
         Soundex soundex(word);
-        return soundex.encodeImpl();
+        return soundex.encode();
     }
 
-    std::string encodeImpl() {
+    std::string encode() {
          encodeInitial();
          encodeWordAfterInitial(wordAfterInitial());
          zeroPad();
@@ -81,22 +81,21 @@ private:
     
     void encodeWordAfterInitial(const std::string& wordAfterInitial) {
     
+        auto lastEncoding = InvalidEncoding;
+        
         for (auto letter : wordAfterInitial) {
             auto nextEncoding = encodeLetter(letter);
             
-            if (isValidEncoding(nextEncoding) &&  nextEncoding != lastEncoding()) {
+            if (isValidEncoding(nextEncoding) && nextEncoding != lastEncoding) {
                 code += nextEncoding;
                 if (isComplete()) break;
     	    }
+    	    
+    	    lastEncoding = nextEncoding;
     	}
     }
 
-   
-    char lastEncoding() const {
-        if (0 == code.length()) return InvalidEncoding;
-        return code[code.length() - 1];
-    }
-
+  
     bool isComplete() const {
         return code.length() >= MaxCodeLength;
     }
@@ -135,6 +134,7 @@ const std::unordered_map<char, char> Soundex::encodings = initial_encodings();
 
 using ::testing::Eq;
 using ::testing::Test;
+using ::testing::StartsWith;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { 
     ASSERT_THAT(Soundex::encode("A"), Eq("A000"));
@@ -246,7 +246,8 @@ TEST(SoundexEncoding, DoesNotCombineInitialDuplicatesWhenSeparatedByVowels) {
 }
 
 TEST(SoundexEncoding, DoesNotCombineDuplicatesWhenSeparatedByVowels) {
-    ASSERT_THAT(Soundex::encode("Rcacdl"), Eq("R234"));
+    ASSERT_THAT(Soundex::encode("Rcacdl"), Eq("R223"));
+    ASSERT_THAT(Soundex::encode("Rracacad"), Eq("R223"));
 }
 
 TEST(SoundexEncoding, CombinesDuplicateInitialVowels) {
@@ -265,3 +266,73 @@ TEST(SoundexEncoding, CombinesDuplicatesToEnd) {
     EXPECT_THAT(Soundex::encode("Aaaa"), Eq("A000"));
 }
 
+
+TEST(SoundexEncodingOther, RetainsSoleLetterOfOneLetterWord) {
+   ASSERT_THAT(Soundex::encode("A"), Eq("A000")); 
+}
+
+TEST(SoundexEncodingOther, PadsWithZerosToEnsureThreeDigits) {
+   ASSERT_THAT(Soundex::encode("I"), Eq("I000"));
+}
+
+TEST(SoundexEncodingOther, ReplacesConsonantsWithAppropriateDigits) {
+   EXPECT_THAT(Soundex::encode("Ab"), Eq("A100"));
+   EXPECT_THAT(Soundex::encode("Ac"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("Ad"), Eq("A300"));
+   EXPECT_THAT(Soundex::encode("Af"), Eq("A100"));
+   EXPECT_THAT(Soundex::encode("Ag"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("Aj"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("Ak"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("Al"), Eq("A400"));
+   EXPECT_THAT(Soundex::encode("Am"), Eq("A500"));
+   EXPECT_THAT(Soundex::encode("An"), Eq("A500"));
+   EXPECT_THAT(Soundex::encode("Ap"), Eq("A100"));
+   EXPECT_THAT(Soundex::encode("Aq"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("Ar"), Eq("A600"));
+   EXPECT_THAT(Soundex::encode("As"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("At"), Eq("A300"));
+   EXPECT_THAT(Soundex::encode("Av"), Eq("A100"));
+   EXPECT_THAT(Soundex::encode("Ax"), Eq("A200"));
+   EXPECT_THAT(Soundex::encode("Az"), Eq("A200"));
+}
+
+TEST(SoundexEncodingOther, ReplacesMultipleConsonantsWithDigits) {
+   ASSERT_THAT(Soundex::encode("Acdl"), Eq("A234"));
+}
+
+TEST(SoundexEncodingOther, LimitsLengthToFourCharacters) {
+   ASSERT_THAT(Soundex::encode("Dcdlb").length(), Eq(4u)); 
+}
+
+TEST(SoundexEncodingOther, IgnoresVowelLikeLetters) {
+   ASSERT_THAT(Soundex::encode("BAaEeIiOoUuHhYycdl"), Eq("B234"));
+}
+
+TEST(SoundexEncodingOther, IgnoresNonAlphabetics) {
+   ASSERT_THAT(Soundex::encode("F987654321%#.=+cdl"), Eq("F234"));
+}
+
+TEST(SoundexEncodingOther, CombinesDuplicateEncodings) {
+   ASSERT_THAT(Soundex::encodeLetter('b'), Eq(Soundex::encodeLetter('f')));
+   ASSERT_THAT(Soundex::encodeLetter('c'), Eq(Soundex::encodeLetter('g')));
+   ASSERT_THAT(Soundex::encodeLetter('d'), Eq(Soundex::encodeLetter('t')));
+
+   ASSERT_THAT(Soundex::encode("Abfcgdt"), Eq("A123"));
+}
+
+TEST(SoundexEncodingOther, UppercasesFirstLetter) {
+   ASSERT_THAT(Soundex::encode("abcd"), StartsWith("A"));
+}
+
+TEST(SoundexEncodingOther, IgnoresCaseWhenEncodingConsonants) {
+   ASSERT_THAT(Soundex::encode("BCDL"), Eq(Soundex::encode("Bcdl")));
+}
+
+TEST(SoundexEncodingOther, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
+   ASSERT_THAT(Soundex::encode("Bbcd"), Eq("B230"));
+}
+
+TEST(SoundexEncodingOther, DoesNotCombineDuplicateEncodingsSeparatedByVowels) {
+   ASSERT_THAT(Soundex::encode("Jbob"), Eq("J110"));
+   ASSERT_THAT(Soundex::encode("Jbaobab"), Eq("J111"));
+}
